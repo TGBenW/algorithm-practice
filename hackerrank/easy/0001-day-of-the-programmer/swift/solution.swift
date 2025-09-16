7b5f4bd00bccd8abde2efeb779b3cef376b89cc7
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-16 22:55:51 +0300

    feat(hackerrank): day-of-the-programmer [swift]

--- hackerrank/easy/0001-day-of-the-programmer/swift/solution.swift
+++ hackerrank/easy/0001-day-of-the-programmer/swift/solution.swift
@@ -1,3 +1,17 @@
 // solution.swift
 
+func dayOfProgrammer(year: Int) -> String {
+    var dayMonth = ""
+    if year == 1918 {
+        dayMonth = "26.09"
+    } else if year < 1918 {
+        dayMonth = year % 4 == 0 ? "12.09" : "13.09"
+    } else {
+        dayMonth = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) ? "12.09" : "13.09"
+    }
+    return "\(dayMonth).\(year)"
+}
 
+// --- demo run ---
+let input = 2017
+print(dayOfProgrammer(year: input))  // 13.09.2017
