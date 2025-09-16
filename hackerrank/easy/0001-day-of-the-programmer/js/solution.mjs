a0d40d661b62b1aa2827566c9108165eb1167448
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-16 23:07:51 +0300

    feat(hackerrank): day-of-the-programmer [js]

--- hackerrank/easy/0001-day-of-the-programmer/js/solution.mjs
+++ hackerrank/easy/0001-day-of-the-programmer/js/solution.mjs
@@ -1,1 +1,17 @@
 // solution.mjs
+
+export function dayOfProgrammer(year) {
+  if (year === 1918) return `26.09.${year}`;
+  if (year < 1918) {
+    return year % 4 === 0 ? `12.09.${year}` : `13.09.${year}`;
+  }
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+    return `12.09.${year}`;
+  }
+  return `13.09.${year}`;
+}
+
+// --- demo run ---
+if (import.meta.url === `file://${process.argv[1]}`) {
+  console.log(dayOfProgrammer(2017)); // 13.09.2017
+}
