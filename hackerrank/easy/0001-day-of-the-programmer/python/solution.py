0dde682df47d56dab1cb4339476362540c19a4b9
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-16 23:21:51 +0300

    feat(hackerrank): day-of-the-programmer [python]

--- hackerrank/easy/0001-day-of-the-programmer/python/solution.py
+++ hackerrank/easy/0001-day-of-the-programmer/python/solution.py
@@ -1,1 +1,17 @@
 // solution.py
+
+def day_of_programmer(year: int) -> str:
+    if year == 1918:
+        return f"26.09.{year}"
+    elif year < 1918:
+        return f"{'12.09' if year % 4 == 0 else '13.09'}.{year}"
+    else:
+        if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+            return f"12.09.{year}"
+        else:
+            return f"13.09.{year}"
+
+
+# --- demo run ---
+if __name__ == "__main__":
+    print(day_of_programmer(2017))  # 13.09.2017
