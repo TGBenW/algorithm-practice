35b2d4fe67bd7adb81a3f8faf673c976a07263e4
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-17 21:26:27 +0300

    feat(hackerrank): the-hurdle-race [python]
    implemented python version with simple max check and demo run
    + minor template fix

--- hackerrank/easy/0002-the-hurdle-race/python/solution.py
+++ hackerrank/easy/0002-the-hurdle-race/python/solution.py
@@ -1,1 +1,14 @@
-// solution.py
+# solution.py
+
+from typing import List
+
+def hurdle_race(k: int, height: List[int]) -> int:
+    max_hurdle = max(height) if height else 0
+    return 0 if k >= max_hurdle else max_hurdle - k
+
+
+# --- demo run ---
+if __name__ == "__main__":
+    k = 4
+    hurdles = [1, 6, 3, 5, 2]
+    print(hurdle_race(k, hurdles))  # 2
