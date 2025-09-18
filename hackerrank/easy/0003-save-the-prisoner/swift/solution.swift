154b03d447dcb3dc031f26e35023ef0827f274e1
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-18 23:01:50 +0300

    feat(hackerrank): save-the-prisoner [swift]
    implemented swift solution using modular arithmetic with demo runs

--- hackerrank/easy/0003-save-the-prisoner/swift/solution.swift
+++ hackerrank/easy/0003-save-the-prisoner/swift/solution.swift
@@ -1,2 +1,12 @@
 // solution.swift
 
+func saveThePrisoner(n: Int, m: Int, s: Int) -> Int {
+    let x = (s + m - 1) % n
+    return x == 0 ? n : x
+}
+
+// --- demo run ---
+print(saveThePrisoner(n: 5, m: 2, s: 1))  // 2
+print(saveThePrisoner(n: 5, m: 2, s: 2))  // 3
+print(saveThePrisoner(n: 7, m: 19, s: 2)) // 6
+print(saveThePrisoner(n: 3, m: 7, s: 3))  // 3
