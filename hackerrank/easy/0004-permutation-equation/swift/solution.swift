068798715c6f4cde071439f711a6d9591bbf7d08
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-19 23:49:36 +0300

    feat(hackerrank): permutation-equation [swift]
    implemented swift solution using index lookups with demo runs

--- hackerrank/easy/0004-permutation-equation/swift/solution.swift
+++ hackerrank/easy/0004-permutation-equation/swift/solution.swift
@@ -1,2 +1,18 @@
 // solution.swift
 
+func permutationEquation(p: [Int]) -> [Int] {
+    var resultArray = [Int]()
+    for x in 1...p.count {
+        let positionOfX = p.firstIndex(of: x)! + 1
+        let y = p.firstIndex(of: positionOfX)! + 1
+        resultArray.append(y)
+    }
+    return resultArray
+}
+
+// --- demo run ---
+let p1 = [2, 3, 1]
+print(permutationEquation(p: p1))  // [2, 3, 1]
+
+let p2 = [4, 3, 5, 1, 2]
+print(permutationEquation(p: p2))  // [1, 3, 5, 4, 2]
