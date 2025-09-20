aeb253e580ad52ac101b1faf64ca530f566d1104
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-20 23:39:49 +0300

    feat(hackerrank): simple-array-sum [swift]
    implemented swift solution using reduce with demo run

--- hackerrank/easy/0005-simple-array-sum/swift/solution.swift
+++ hackerrank/easy/0005-simple-array-sum/swift/solution.swift
@@ -1,2 +1,9 @@
 // solution.swift
 
+func simpleArraySum(ar: [Int]) -> Int {
+    return ar.reduce(0, +)
+}
+
+// --- demo run ---
+let arr = [1, 2, 3, 4, 10, 11]
+print(simpleArraySum(ar: arr))  // 31
