36fad7d960992c140f88c9baabbfe41db1ea3236
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-21 23:29:03 +0300

    feat(hackerrank): compare-the-triplets [swift]
    implemented swift solution with straightforward comparisons and demo runs

--- hackerrank/easy/0006-compare-the-triplets/swift/solution.swift
+++ hackerrank/easy/0006-compare-the-triplets/swift/solution.swift
@@ -1,2 +1,14 @@
 // solution.swift
 
+func compareTriplets(a: [Int], b: [Int]) -> [Int] {
+    var res = [0, 0] // [Alice, Bob]
+    for i in 0..<a.count {
+        if a[i] > b[i] { res[0] += 1 }
+        else if a[i] < b[i] { res[1] += 1 }
+    }
+    return res
+}
+
+// --- demo run ---
+print(compareTriplets(a: [5,6,7], b: [3,6,10]))   // [1, 1]
+print(compareTriplets(a: [17,28,30], b: [99,16,8])) // [2, 1]
