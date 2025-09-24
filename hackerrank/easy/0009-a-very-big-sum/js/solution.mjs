2073cf8c529e231ea6e93fde5197737de96cdd46
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-24 23:38:46 +0300

    feat(hackerrank): a-very-big-sum [js]
    added javascript solution using array.reduce; prints sample sum

--- hackerrank/easy/0009-a-very-big-sum/js/solution.mjs
+++ hackerrank/easy/0009-a-very-big-sum/js/solution.mjs
@@ -1,2 +1,8 @@
 // solution.mjs
 
+export function aVeryBigSum(ar) {
+  return ar.reduce((acc, x) => acc + x, 0);
+}
+
+// --- demo run ---
+console.log(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005])); // 5000000015
