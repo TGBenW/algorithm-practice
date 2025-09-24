5e65b5a2d3b8c843fac98d0f005eb492e6528b56
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-24 23:38:09 +0300

    feat(hackerrank): a-very-big-sum [swift]
    implemented swift solution using reduce; demo with large values

--- hackerrank/easy/0009-a-very-big-sum/swift/solution.swift
+++ hackerrank/easy/0009-a-very-big-sum/swift/solution.swift
@@ -1,2 +1,9 @@
 // solution.swift
 
+func aVeryBigSum(ar: [Int]) -> Int {
+    return ar.reduce(0, +)
+}
+
+// --- demo run ---
+let arr = [1_000_000_001, 1_000_000_002, 1_000_000_003, 1_000_000_004, 1_000_000_005]
+print(aVeryBigSum(ar: arr))  // 5000000015
