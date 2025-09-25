079eb09d136414f443e5ba78f4958c7fc590988c
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-25 22:42:33 +0300

    feat(hackerrank): diagonal-difference [swift]
    implemented swift solution using diagonal traversal with demo run

--- hackerrank/easy/0010-diagonal-difference/swift/solution.swift
+++ hackerrank/easy/0010-diagonal-difference/swift/solution.swift
@@ -1,2 +1,19 @@
 // solution.swift
 
+func diagonalDifference(arr: [[Int]]) -> Int {
+    var leftDiagonal = 0
+    var rightDiagonal = 0
+    for i in 0..<arr.count {
+        leftDiagonal += arr[i][i]
+        rightDiagonal += arr[i][arr.count - 1 - i]
+    }
+    return abs(leftDiagonal - rightDiagonal)
+}
+
+// --- demo run ---
+let matrix = [
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12]
+]
+print(diagonalDifference(arr: matrix))  // 15
