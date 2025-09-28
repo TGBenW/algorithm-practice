f9152cc0b9168c4e11b825e9576b47a34edc2fa2
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-28 22:12:32 +0300

    feat(hackerrank): bon-appetit [python]
    implemented python solution excluding index k and printing refund or 'Bon Appetit'

--- hackerrank/easy/0013-bon-appetit/python/solution.py
+++ hackerrank/easy/0013-bon-appetit/python/solution.py
@@ -1,2 +1,13 @@
 ## solution.py
 
+from typing import List
+
+def bonAppetit(bill: List[int], k: int, b: int) -> None:
+    anna = (sum(price for i, price in enumerate(bill) if i != k)) // 2
+    print("Bon Appetit" if anna == b else b - anna)
+
+
+# --- demo run ---
+if __name__ == "__main__":
+    bonAppetit([3, 10, 2, 9], 1, 12)  # 5
+    bonAppetit([3, 10, 2, 9], 1, 7)   # Bon Appetit
