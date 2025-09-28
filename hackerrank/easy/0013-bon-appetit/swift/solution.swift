ff6197fd4b794def5c5c9bc546d8b90624477481
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-28 22:11:27 +0300

    feat(hackerrank): bon-appetit [swift]
    implemented swift solution with exclusion by index and demo runs

--- hackerrank/easy/0013-bon-appetit/swift/solution.swift
+++ hackerrank/easy/0013-bon-appetit/swift/solution.swift
@@ -1,2 +1,19 @@
 // solution.swift
 
+func bonAppetit(bill: [Int], k: Int, b: Int) {
+    // Sum all items except the one Anna didn't eat, then split by 2
+    var sum = 0
+    for (idx, price) in bill.enumerated() {
+        if idx != k { sum += price }
+    }
+    let annasShare = sum / 2
+    if annasShare == b {
+        print("Bon Appetit")
+    } else {
+        print(b - annasShare)
+    }
+}
+
+// --- demo run ---
+bonAppetit(bill: [3, 10, 2, 9], k: 1, b: 12) // 5
+bonAppetit(bill: [3, 10, 2, 9], k: 1, b: 7)  // Bon Appetit
