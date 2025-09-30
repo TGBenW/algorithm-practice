c7a38a39b59cd8fe165a64debe48887ba4026f91
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-09-30 23:13:39 +0300

    feat(hackerrank): drawing-book [swift]
    implemented swift solution with min(front, back) formula and demo run

--- hackerrank/easy/0015-drawing-book/swift/solution.swift
+++ hackerrank/easy/0015-drawing-book/swift/solution.swift
@@ -1,2 +1,9 @@
 // solution.swift
 
+func pageCount(n: Int, p: Int) -> Int {
+    return min(p / 2, (n / 2) - (p / 2))
+}
+
+// --- demo run ---
+print(pageCount(n: 6, p: 2))  // 1
+print(pageCount(n: 5, p: 4))  // 0
