6780cd404a0ce517ffb44acd63b27271ac260b82
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-10-05 23:35:54 +0300

    feat(hackerrank): picking-numbers [swift]
    implemented frequency-based solution: max over freq[x]+freq[x+1]; added demo runs

--- hackerrank/easy/0019-picking-numbers/swift/solution.swift
+++ hackerrank/easy/0019-picking-numbers/swift/solution.swift
@@ -1,2 +1,16 @@
 // solution.swift
 
+func pickingNumbers(a: [Int]) -> Int {
+    var freq: [Int: Int] = [:]
+    for v in a { freq[v, default: 0] += 1 }
+    var best = 0
+    for (x, cnt) in freq {
+        let withNext = cnt + (freq[x + 1] ?? 0)
+        if withNext > best { best = withNext }
+    }
+    return best
+}
+
+// --- demo run ---
+print(pickingNumbers(a: [4, 6, 5, 3, 3, 1]))      // 3
+print(pickingNumbers(a: [1, 2, 2, 3, 1, 2]))      // 5
