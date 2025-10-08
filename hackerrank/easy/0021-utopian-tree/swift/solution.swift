7b54ebaf66eb5bdc5eb71c33dab59c462916720f
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-10-08 23:36:06 +0300

    feat(hackerrank): utopian-tree [swift]
    implemented cycle-based simulation (spring: *2, summer: +1) starting from height=1; added demo runs

--- hackerrank/easy/0021-utopian-tree/swift/solution.swift
+++ hackerrank/easy/0021-utopian-tree/swift/solution.swift
@@ -1,2 +1,20 @@
 // solution.swift
 
+func utopianTree(n: Int) -> Int {
+    var h = 1
+    if n == 0 { return h }
+    for cycle in 1...n {
+        if cycle % 2 == 1 { // spring (odd)
+            h *= 2
+        } else {            // summer (even)
+            h += 1
+        }
+    }
+    return h
+}
+
+// --- demo run ---
+print(utopianTree(n: 0))  // 1
+print(utopianTree(n: 1))  // 2
+print(utopianTree(n: 4))  // 7
+print(utopianTree(n: 5))  // 14
