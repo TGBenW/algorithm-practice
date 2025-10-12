78b4373af46c5a8caca717d44532111733779c9a
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-10-12 23:11:51 +0300

    feat(hackerrank): beautiful-days-at-the-movies [js]
    added javascript implementation with digit reversal and modular condition; included demo runs

--- hackerrank/easy/0022-beautiful-days-at-the-movies/js/solution.mjs
+++ hackerrank/easy/0022-beautiful-days-at-the-movies/js/solution.mjs
@@ -1,2 +1,19 @@
 // solution.mjs
 
+export function beautifulDays(i, j, k) {
+  function reverseNumber(num) {
+    return parseInt(num.toString().split('').reverse().join(''), 10);
+  }
+
+  let count = 0;
+  for (let day = i; day <= j; day++) {
+    if (Math.abs(day - reverseNumber(day)) % k === 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// --- demo run ---
+console.log(beautifulDays(20, 23, 6)); // 2
+console.log(beautifulDays(10, 13, 3)); // 2
