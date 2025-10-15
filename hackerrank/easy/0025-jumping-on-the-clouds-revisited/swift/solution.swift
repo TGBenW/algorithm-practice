8521e28aefabca973c406b932a7362f6b2f83196
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-10-15 23:26:21 +0300

    feat(hackerrank): jumping-on-the-clouds-revisited [swift]
    implemented cyclic simulation with modulo; each jump -1 energy, thundercloud -3 total; added sample demo

--- hackerrank/easy/0025-jumping-on-the-clouds-revisited/swift/solution.swift
+++ hackerrank/easy/0025-jumping-on-the-clouds-revisited/swift/solution.swift
@@ -1,2 +1,15 @@
 // solution.swift
 
+func jumpingOnClouds(c: [Int], k: Int) -> Int {
+    var energy = 100
+    var pos = 0
+    repeat {
+        pos = (pos + k) % c.count
+        energy -= (c[pos] == 1) ? 3 : 1
+    } while pos != 0
+    return energy
+}
+
+// --- demo run (from sample) ---
+let c0 = [0, 0, 1, 0, 0, 1, 1, 0]
+print(jumpingOnClouds(c: c0, k: 2)) // 92
