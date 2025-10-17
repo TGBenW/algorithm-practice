66e57197c0e5aa880b1422280b506fc70951c6d5
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-10-17 23:54:00 +0300

    feat(hackerrank): extra-long-factorials [python]
    implemented factorial with Python big ints; prints result; added demo for 10 and 25

--- hackerrank/medium/0002-extra-long-factorials/python/solution.py
+++ hackerrank/medium/0002-extra-long-factorials/python/solution.py
@@ -1,2 +1,13 @@
 ## solution.py
 
+def extra_long_factorials(n: int) -> None:
+    res = 1
+    for i in range(2, n + 1):
+        res *= i
+    print(res)
+
+
+# --- demo run ---
+if __name__ == "__main__":
+    extra_long_factorials(10)  # 3628800
+    extra_long_factorials(25)  # 15511210043330985984000000
