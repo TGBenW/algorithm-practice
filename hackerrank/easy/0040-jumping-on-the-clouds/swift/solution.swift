c08ab893b6a7b508aca4321f29f24dec26909d2d
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-11-02 22:06:32 +0200

    feat(hackerrank): jumping-on-the-clouds [swift]
    implemented greedy jumping logic prioritizing double steps when possible; included sample demo runs for verification

--- hackerrank/easy/0040-jumping-on-the-clouds/swift/solution.swift
+++ hackerrank/easy/0040-jumping-on-the-clouds/swift/solution.swift
@@ -1,2 +1,21 @@
 // solution.swift
 
+import Foundation
+
+func jumpingOnClouds(c: [Int]) -> Int {
+    var jumps = 0
+    var i = 0
+    while i < c.count - 1 {
+        if i + 2 < c.count && c[i + 2] == 0 {
+            i += 2
+        } else {
+            i += 1
+        }
+        jumps += 1
+    }
+    return jumps
+}
+
+// --- demo runs ---
+print(jumpingOnClouds(c: [0, 0, 1, 0, 0, 1, 0])) // 4
+print(jumpingOnClouds(c: [0, 0, 0, 0, 1, 0]))    // 3
