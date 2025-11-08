4410abfa8e47fd84462602a619f5710f321efc17
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-11-08 21:00:55 +0200

    feat(hackerrank): staircase [swift]
    implemented right-aligned staircase printer; added demo run (n=6) with expected output

--- hackerrank/easy/0043-staircase/swift/solution.swift
+++ hackerrank/easy/0043-staircase/swift/solution.swift
@@ -1,2 +1,24 @@
 // solution.swift
 
+import Foundation
+
+func staircase(n: Int) {
+    guard n > 0 else { return }
+    for i in 1...n {
+        let spaces = String(repeating: " ", count: n - i)
+        let hashes = String(repeating: "#", count: i)
+        print(spaces + hashes)
+    }
+}
+
+// --- demo run ---
+staircase(n: 6)
+/*
+Expected:
+     #
+    ##
+   ###
+  ####
+ #####
+######
+*/
