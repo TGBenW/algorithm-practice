835fafec60056823742b020a08fbf96f33d3d326
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-11-12 22:44:14 +0200

    feat(hackerrank): tutorial-intro [swift]
    implemented binary search to locate target V in sorted array
    added demo runs for multiple targets (expected 1, 4, 5)

--- hackerrank/easy/0047-tutorial-intro/swift/solution.swift
+++ hackerrank/easy/0047-tutorial-intro/swift/solution.swift
@@ -1,2 +1,24 @@
 // solution.swift
 
+func introTutorial(V: Int, arr: [Int]) -> Int {
+    var left = 0
+    var right = arr.count - 1
+
+    while left <= right {
+        let mid = (left + right) / 2
+        if arr[mid] == V {
+            return mid
+        } else if arr[mid] < V {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return -1
+}
+
+// --- demo run ---
+let arr = [1, 4, 5, 7, 9, 12]
+print(introTutorial(V: 4, arr: arr)) // Expected: 1
+print(introTutorial(V: 9, arr: arr)) // Expected: 4
+print(introTutorial(V: 12, arr: arr)) // Expected: 5
