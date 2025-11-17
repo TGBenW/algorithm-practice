1e7a58256954a646126a8f56493e70997295f294
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-11-17 23:39:29 +0200

    feat(hackerrank): chocolate-feast [swift]
    implemented Swift simulation of chocolate purchases and wrapper exchanges
    added demo runs that cover all sample test cases from the problem statement

--- hackerrank/easy/0050-chocolate-feast/swift/solution.swift
+++ hackerrank/easy/0050-chocolate-feast/swift/solution.swift
@@ -1,2 +1,28 @@
 // solution.swift
 
+import Foundation
+
+func chocolateFeast(n: Int, c: Int, m: Int) -> Int {
+    var totalChocolates = n / c
+    var wrappers = totalChocolates
+
+    while wrappers >= m {
+        let newChocolates = wrappers / m
+        totalChocolates += newChocolates
+        wrappers = wrappers % m + newChocolates
+    }
+
+    return totalChocolates
+}
+
+// --- demo run with sample input ---
+let t = 3
+let tests: [(Int, Int, Int)] = [
+    (10, 2, 5), // Expected: 6
+    (12, 4, 4), // Expected: 3
+    (6, 2, 2)   // Expected: 5
+]
+
+for (n, c, m) in tests {
+    print(chocolateFeast(n: n, c: c, m: m))
+}
