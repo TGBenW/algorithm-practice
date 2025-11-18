7266903c22d3d1ee592914088f2bdea96b24a161
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-11-18 22:57:04 +0200

    feat(hackerrank): acm-icpc-team [python]
    implemented Python solution to evaluate all 2-person teams and track max topics plus team count
    added demo run using the provided sample expecting [5, 2]

--- hackerrank/easy/0051-acm-icpc-team/python/solution.py
+++ hackerrank/easy/0051-acm-icpc-team/python/solution.py
@@ -1,2 +1,42 @@
 ## solution.py
 
+from typing import List, Tuple
+
+
+def acmTeam(topic: List[str]) -> List[int]:
+    n = len(topic)
+    if n < 2:
+        return [0, 0]
+    m = len(topic[0])
+
+    max_known = 0
+    teams_count = 0
+
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            known_for_this_combo = 0
+
+            # count topics known by at least one of the two attendees
+            for k in range(m):
+                if topic[i][k] == "1" or topic[j][k] == "1":
+                    known_for_this_combo += 1
+
+            if known_for_this_combo > max_known:
+                max_known = known_for_this_combo
+                teams_count = 1
+            elif known_for_this_combo == max_known:
+                teams_count += 1
+
+    return [max_known, teams_count]
+
+
+# --- demo run ---
+if __name__ == "__main__":
+    sample_topics = [
+        "10101",
+        "11100",
+        "11010",
+        "00101",
+    ]
+    # Expected: [5, 2]
+    print(acmTeam(sample_topics))
