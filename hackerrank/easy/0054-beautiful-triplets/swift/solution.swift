7c2a6ec9230b7ebdb5a121678366c3f1a361ab02
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-12-02 23:57:11 +0200

    feat(hackerrank): beautiful-triplets [swift]
    implemented Swift solution counting beautiful triplets with a single pass and value-frequency dictionary
    added demo run for the sample input [1, 2, 4, 5, 7, 8, 10] with d=3 expecting result 3

--- hackerrank/easy/0054-beautiful-triplets/swift/solution.swift
+++ hackerrank/easy/0054-beautiful-triplets/swift/solution.swift
@@ -1,2 +1,24 @@
 // solution.swift
 
+import Foundation
+
+func beautifulTriplets(d: Int, arr: [Int]) -> Int {
+    var tripletCount = 0
+    var countDict = [Int: Int]()
+
+    for number in arr {
+        if let count2 = countDict[number - 2 * d],
+           let count1 = countDict[number - d] {
+            tripletCount += count1 * count2
+        }
+        countDict[number, default: 0] += 1
+    }
+
+    return tripletCount
+}
+
+// --- demo run ---
+let dSample = 3
+let arrSample = [1, 2, 4, 5, 7, 8, 10]
+// Expected: 3
+print(beautifulTriplets(d: dSample, arr: arrSample))
