53fd69cf538357c7be57b6f4789cab33998536db
Andrey <32848273+TGBenW@users.noreply.github.com>
2025-12-07 23:37:11 +0200

    feat(hackerrank): camelcase [swift]
    implemented Swift CamelCase word counter by counting uppercase characters plus the first word
    added demo run for saveChangesInTheEditor expecting result 5

--- hackerrank/easy/0058-camelcase/swift/solution.swift
+++ hackerrank/easy/0058-camelcase/swift/solution.swift
@@ -1,2 +1,19 @@
 // solution.swift
 
+import Foundation
+
+func camelcase(s: String) -> Int {
+    guard !s.isEmpty else { return 0 }
+
+    var result = 1
+    for char in s {
+        if char.isUppercase {
+            result += 1
+        }
+    }
+    return result
+}
+
+// --- demo run ---
+let sample = "saveChangesInTheEditor" // Expected: 5
+print(camelcase(s: sample))
